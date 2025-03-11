e26cccb79bb1523a8dcfa103c25314a0dc5f7a00
cqlyj <luoyingjie0721@gmail.com>
2025-03-11 19:43:21 +0800

    feat: add AddEmployee script for managing employee additions in Payroll contract

---
+++ script/AddEmployee.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {Payroll} from "src/Payroll.sol";
+
+contract AddEmployee is Script {
+    address payroll;
+    address constant ANVIL_SECOND_ADDRESS =
+        0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
+
+    function addEmployee(address mostRecentlyDeployment) public {
+        vm.startBroadcast();
+        Payroll(mostRecentlyDeployment).addEmployee(
+            1,
+            ANVIL_SECOND_ADDRESS,
+            1e10,
+            block.timestamp,
+            30 seconds
+        );
+        vm.stopBroadcast();
+        console.log("We have the employee added");
+    }
+
+    function run() external {
+        payroll = Vm(address(vm)).getDeployment(
+            "Payroll",
+            uint64(block.chainid)
+        );
+
+        addEmployee(payroll);
+    }
+}
