9dc0a895a7c710f3f6fcf62c60e5dc6cebc7e0a0
cqlyj <luoyingjie0721@gmail.com>
2025-03-11 19:48:12 +0800

    feat: add SetTokenPreferences script for configuring token preferences in Payroll contract

---
+++ script/SetTokenPreferences.s.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {Payroll} from "src/Payroll.sol";
+
+contract SetTokenPreferences is Script {
+    address payroll;
+    address constant LINK_ADDRESS = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
+
+    function setTokenPreferences(address mostRecentlyDeployment) public {
+        address[] memory tokens = new address[](1);
+        tokens[0] = LINK_ADDRESS;
+        uint256[] memory percentages = new uint256[](1);
+        percentages[0] = 50;
+
+        vm.startBroadcast();
+        Payroll(mostRecentlyDeployment).setTokenPreferences(
+            1,
+            tokens,
+            percentages
+        );
+        vm.stopBroadcast();
+        console.log("Token preferences have been set");
+    }
+
+    function run() external {
+        payroll = Vm(address(vm)).getDeployment(
+            "Payroll",
+            uint64(block.chainid)
+        );
+
+        setTokenPreferences(payroll);
+    }
+}
