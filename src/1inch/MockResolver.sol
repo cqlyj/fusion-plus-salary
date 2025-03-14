78a40fea7439f0d6c9289239bb052c2b93374b7c
cqlyj <luoyingjie0721@gmail.com>
2025-03-14 22:22:15 +0800

    feat: implement Ownable functionality in MockResolver and add deploySrc method for escrow contract deployment

--- src/1inch/MockResolver.sol
+++ src/1inch/MockResolver.sol
@@ -8,8 +8,10 @@ import {IEscrowSrc} from "cross-chain-swap/contracts/interfaces/IEscrowSrc.sol";
 import {IEscrowDst} from "cross-chain-swap/contracts/interfaces/IEscrowDst.sol";
 import {IBaseEscrow} from "cross-chain-swap/contracts/interfaces/IBaseEscrow.sol";
 import {TakerTraits} from "limit-order-protocol/libraries/TakerTraitsLib.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {TimelocksLib} from "cross-chain-swap/contracts/libraries/TimelocksLib.sol";
 
-contract MockResolver {
+contract MockResolver is Ownable {
     // https://portal.1inch.dev/documentation/apis/swap/fusion-plus/swagger/orders?method=get&path=%2Fv1.0%2Forder%2Fescrow
     IEscrowFactory immutable i_escrowFactory =
         IEscrowFactory(0xa7bCb4EAc8964306F9e3764f67Db6A7af6DdF99A);
@@ -17,26 +19,45 @@ contract MockResolver {
     IOrderMixin immutable i_order =
         IOrderMixin(0x111111125421cA6dc452d289314280a0f8842A65);
 
-    function addressOfEscrowSrc(
-        IBaseEscrow.Immutables calldata immutables
-    ) external view virtual returns (address) {
-        return i_escrowFactory.addressOfEscrowSrc(immutables);
-    }
+    constructor() Ownable(msg.sender) {}
 
-    function addressOfEscrowDst(
-        IBaseEscrow.Immutables calldata immutables
-    ) external view virtual returns (address) {
-        return i_escrowFactory.addressOfEscrowDst(immutables);
-    }
-
-    function createDstEscrow(
-        IBaseEscrow.Immutables calldata dstImmutables,
-        uint256 srcCancellationTimestamp
-    ) external payable {
-        i_escrowFactory.createDstEscrow{value: msg.value}(
-            dstImmutables,
-            srcCancellationTimestamp
+    /**
+     * @notice Deploys a new escrow contract for maker on the source chain.
+     * @param immutables The immutables of the escrow contract that are used in deployment.
+     * @param order Order quote to fill.
+     * @param r R component of signature.
+     * @param vs VS component of signature.
+     * @param amount Taker amount to fill
+     * @param takerTraits Specifies threshold as maximum allowed takingAmount when takingAmount is zero, otherwise specifies
+     * minimum allowed makingAmount. The 2nd (0 based index) highest bit specifies whether taker wants to skip maker's permit.
+     * @param args Arguments that are used by the taker (target, extension, interaction, permit).
+     */
+    function deploySrc(
+        IBaseEscrow.Immutables calldata immutables,
+        IOrderMixin.Order calldata order,
+        bytes32 r,
+        bytes32 vs,
+        uint256 amount,
+        TakerTraits takerTraits,
+        bytes calldata args
+    ) external onlyOwner {
+        IBaseEscrow.Immutables memory immutablesMem = immutables;
+        immutablesMem.timelocks = TimelocksLib.setDeployedAt(
+            immutables.timelocks,
+            block.timestamp
         );
+        address computed = i_escrowFactory.addressOfEscrowSrc(immutablesMem);
+        (bool success, ) = address(computed).call{
+            value: immutablesMem.safetyDeposit
+        }("");
+        if (!success) revert IBaseEscrow.NativeTokenSendingFailure();
+
+        // _ARGS_HAS_TARGET = 1 << 251
+        takerTraits = TakerTraits.wrap(
+            TakerTraits.unwrap(takerTraits) | uint256(1 << 251)
+        );
+        bytes memory argsMem = abi.encodePacked(computed, args);
+        i_order.fillOrderArgs(order, r, vs, amount, takerTraits, argsMem);
     }
 
     /**
@@ -52,19 +73,35 @@ contract MockResolver {
      * @return takingAmount Actual amount transferred from taker to maker
      * @return orderHash Hash of the filled order
      */
-    function fillOrderArgs(
-        IOrderMixin.Order calldata order,
-        bytes32 r,
-        bytes32 vs,
-        uint256 amount,
-        TakerTraits takerTraits,
-        bytes calldata args
-    )
-        external
-        payable
-        returns (uint256 makingAmount, uint256 takingAmount, bytes32 orderHash)
-    {
-        return i_order.fillOrderArgs(order, r, vs, amount, takerTraits, args);
+    // function fillOrderArgs(
+    //     IOrderMixin.Order calldata order,
+    //     bytes32 r,
+    //     bytes32 vs,
+    //     uint256 amount,
+    //     TakerTraits takerTraits,
+    //     bytes calldata args
+    // )
+    //     external
+    //     payable
+    //     returns (uint256 makingAmount, uint256 takingAmount, bytes32 orderHash)
+    // {
+    //     return i_order.fillOrderArgs(order, r, vs, amount, takerTraits, args);
+    // }
+
+    function addressOfEscrowDst(
+        IBaseEscrow.Immutables calldata immutables
+    ) external view virtual returns (address) {
+        return i_escrowFactory.addressOfEscrowDst(immutables);
+    }
+
+    function createDstEscrow(
+        IBaseEscrow.Immutables calldata dstImmutables,
+        uint256 srcCancellationTimestamp
+    ) external payable {
+        i_escrowFactory.createDstEscrow{value: msg.value}(
+            dstImmutables,
+            srcCancellationTimestamp
+        );
     }
 
     /**
